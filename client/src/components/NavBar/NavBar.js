1fd9e3d1c8c33e7b4527df5ccdafa9c4cef4091a
kartikey sood <kartikeysood@rediffmail.com>
2022-03-15 16:33:12 +0530

    added wallet btn and balance

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import classes from "./NavBar.module.css";
 import { Navbar, Nav, Container } from "react-bootstrap";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWallet, faIndianRupeeSign } from "@fortawesome/free-solid-svg-icons";
 const NavigationBar = () => {
   return (
     <Navbar
@@ -17,10 +18,25 @@ const NavigationBar = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Resources</Nav.Link>
+            <Nav.Link href="#pricing">Level</Nav.Link>
           </Nav>
           <Nav>
+            <div className={classes.balance}>
+              <FontAwesomeIcon icon={faIndianRupeeSign}></FontAwesomeIcon> 10000
+            </div>
+            <Nav.Link
+              href="#deets"
+              className={classes.walletbtn}
+              style={{ color: "white" }}
+            >
+              <FontAwesomeIcon
+                icon={faWallet}
+                className={classes.walleticon}
+              ></FontAwesomeIcon>
+              Wallet
+            </Nav.Link>
+
             <Nav.Link
               href="#deets"
               className={classes.login}
